746a77f41fcf6ecb8866f4e5b5361512001ea3f9
Ansh Babbar <ansh28babbar@gmail.com>
2026-04-29 02:54:54 +0530

    Initial proxy setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://YOUR-SITE-NAME.framer.website/:path*',
+      },
+    ];
+  },
+  // Optional: speeds up Vercel deployments since this is just a proxy
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
